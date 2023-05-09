41a22a5f3059bca1a462b138656b0213ee5b06bd
dskwak <dskwak@tlksys.com>
2023-05-09 16:40:53 +0900

    send all platforms

--- src/main/java/com/example/api/controller/FcmController.java
+++ src/main/java/com/example/api/controller/FcmController.java
@@ -5,9 +5,7 @@ import com.google.api.services.storage.Storage;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.*;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.apache.http.HttpResponse;
@@ -31,8 +29,8 @@ import com.google.firebase.messaging.FirebaseMessaging;
 @CrossOrigin(origins = "*")
 public class FcmController {
 
-    @PostMapping(value = "/send", produces = "application/json")
-    public String send(@RequestBody SendMessageRequest request) throws URISyntaxException, IOException {
+    @PostMapping(value = "/send-rest", produces = "application/json")
+    public String sendRest(@RequestBody SendMessageRequest request) throws URISyntaxException, IOException {
 
         GoogleCredentials googleCredentials =
                 GoogleCredentials.fromStream(new ClassPathResource("private_key.json").getInputStream())
@@ -75,8 +73,8 @@ public class FcmController {
         return result;
     }
 
-    @PostMapping(value = "/send-v2", produces = "application/json")
-    public String sendVer2(@RequestBody SendMessageRequest request) throws FirebaseMessagingException {
+    @PostMapping(value = "/send-admin-sdk", produces = "application/json")
+    public String sendAdminSdk(@RequestBody SendMessageRequest request) throws FirebaseMessagingException {
 
         Notification notification = Notification.builder()
                 .setTitle(request.getTitle())
@@ -90,8 +88,75 @@ public class FcmController {
                 .setNotification(notification)
                 .build();
 
-        String response = FirebaseMessaging.getInstance(FirebaseApp.getInstance()).send(message);
+        String response = FirebaseMessaging.getInstance().send(message);
         System.out.println(response);
+        return response;
+    }
+
+    @PostMapping(value = "/subscribe", produces = "application/json")
+    public String subscribe(@RequestBody List<String> tokenList) throws FirebaseMessagingException {
+
+        TopicManagementResponse topicResponse = FirebaseMessaging.getInstance()
+                .subscribeToTopic(tokenList, "news");
+        System.out.println("success subscribe: "+topicResponse.getSuccessCount());
+        System.out.println("fail subscribe: "+topicResponse.getFailureCount());
+        return topicResponse.toString();
+    }
+
+    @PostMapping(value = "/unsubscribe", produces = "application/json")
+    public String unsubscribe(@RequestBody List<String> tokenList) throws FirebaseMessagingException {
+
+        TopicManagementResponse topicResponse = FirebaseMessaging.getInstance()
+                .unsubscribeFromTopic(tokenList, "news");
+        System.out.println("success unsubscribe: "+topicResponse.getSuccessCount());
+        System.out.println("fail unsubscribe: "+topicResponse.getFailureCount());
+        return topicResponse.toString();
+    }
+
+    @PostMapping(value = "/send-admin-sdk-all", produces = "application/json")
+    public String sendAllPlatforms(@RequestBody SendMessageRequest request) throws FirebaseMessagingException {
+
+        // WebPushNotification, AndroidNotification, Aps 세팅 시 기존 Notification 메세지를 덮어 씀
+        Notification notification = Notification.builder()
+                .setTitle(request.getTitle())
+                .setBody(request.getContent())
+                .build();
+
+        WebpushNotification wpn = WebpushNotification.builder()
+                .setTitle("web push notification")
+                .setBody("push for web")
+                .build();
+
+        AndroidNotification adn = AndroidNotification.builder()
+                .setTitle("android notification")
+                .setBody("push for android")
+                .build();
+
+        Aps aps = Aps.builder()
+                .setAlert("iOS notification")
+                .build();
+
+        Message message = Message.builder()
+                .setToken(request.getToken())
+                .setNotification(notification)
+                .setTopic("news")
+                .setWebpushConfig(WebpushConfig.builder()
+                        .setNotification(wpn)
+                        .build()
+                )
+                .setAndroidConfig(AndroidConfig.builder()
+                        .setNotification(adn)
+                        .build()
+                )
+                .setApnsConfig(ApnsConfig.builder()
+                        .setAps(aps)
+                        .build()
+                )
+                .build();
+
+        String response = FirebaseMessaging.getInstance().send(message);
+        System.out.println(response);
+
         return response;
     }
 
@@ -129,7 +194,7 @@ public class FcmController {
                 .setNotification(notification)
                 .build();
 
-        String response = FirebaseMessaging.getInstance(FirebaseApp.getInstance()).send(message, true);
+        String response = FirebaseMessaging.getInstance().send(message, true);
         System.out.println(response);
         return response;
     }
